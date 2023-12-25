392f1cc110f6c562219899f69bdbef891086a574
zhujiu1207 <100284559+zhujiu1207@users.noreply.github.com>
2023-12-25 17:36:55 +0800

    index.js

--- plugins/doubleclick-tools/index.js
+++ plugins/doubleclick-tools/index.js
@@ -2,20 +2,15 @@
 (function () {
     const plugin = BF2042Portal.Plugins.getPlugin("doubleclick-tools");
 
-    // 设置展开时需要的密码
-    const requiredPassword = "your_password_here";
-
-    // 存储用户输入的密码
-    let enteredPassword = "";
+    // 添加密码
+    const correctPassword = "your_secret_password";
 
     plugin.initializeWorkspace = function () {
         prepareWorkspace();
     };
 
     function prepareWorkspace() {
-        /** @type import("blockly").WorkspaceSvg */
         let workspace = _Blockly.getMainWorkspace();
-
         let ctrlKey = false;
         let lastClickTime = undefined;
 
@@ -30,26 +25,21 @@
         workspace.addChangeListener(function (e) {
             if (e.type === _Blockly.Events.CLICK && e.targetType === "block") {
                 if (lastClickTime && Date.now() - lastClickTime < 200) {
-                    /** @type import("blockly").BlockSvg */
                     const block = workspace.getBlockById(e.blockId);
 
                     if (ctrlKey) {
                         block.setInputsInline(!block.getInputsInline());
                     } else {
-                        // 如果块处于展开状态，要求输入密码
-                        if (!block.isCollapsed()) {
-                            const inputPassword = prompt("Enter the password to expand the block:");
-
+                        const isCollapsed = block.isCollapsed();
+                        if (!isCollapsed) {
                             // 验证密码
-                            if (inputPassword === requiredPassword) {
-                                block.setCollapsed(false);
-                            } else {
-                                alert("Incorrect password. Block remains collapsed.");
+                            const userInput = prompt("请输入密码:");
+                            if (userInput !== correctPassword) {
+                                alert("密码错误，无法展开块！");
+                                return;
                             }
-                        } else {
-                            // 如果块处于折叠状态，直接切换折叠状态
-                            block.setCollapsed(!block.isCollapsed());
                         }
+                        block.setCollapsed(!isCollapsed);
                     }
                 }
 
